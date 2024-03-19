4cf4b05db60d4eb5594ba1e307313dad11b532ce
santhosh katta <santhoshkatta@lyrostech.com>
2024-03-19 14:19:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { useState, createContext, useContext } from "react";
-import Login from "./Login";
-import Register from "./Register";
-import Home from "./Home";
-export const UserContext = createContext();
+import React, {useContext}  from 'react'
+import { UserContext } from './components/UserContext/Usercontext'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './components/home/Home'
+import Login from './components/user/Login'
+import Register from './components/user/Register'
+import Feeds from './components/feeds/Feeds';
+import Albums from './components/albums/Albums';
+
+
+// We should call login comp and register 
 export default function App() {
-  const [user, setUser] = useState("John");
-  const [flag, setFlag] = useState(0);
+    const  {flag, setFlag} =useContext(UserContext);
 
   return (
-    <>
-      <UserContext.Provider value={{ user, setUser, flag, setFlag }}>
-
-        {!flag ? <Login /> : flag == 1 ? <Home /> : flag == 2 ? <Register /> : <Login />}
-
-      </UserContext.Provider>
-    </>
-  );
+    <div>
+        <Router>
+        {flag == 0 || flag==2 ? <Login/>:<Home />}
+        {flag ===2 && <Register/>}
+        
+        <Routes>
+            <Route path="/feeds" index element={<Feeds />}></Route>
+            <Route path="/post" element={<Albums />}></Route>
+        </Routes>
+        </Router>
+        
+    </div>
+  )
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../UserContext/Usercontext';
+import { Link } from 'react-router-dom';
+
+import './Home.css'
+
+function Home() {
+    const {flag , setFlag, name} =useContext(UserContext);
+  return (
+    <div className='top-nav-container'>
+        <div className='nav-links'>
+            <Link to="/feeds">Feeds</Link>
+            <Link to="/albums">Albums</Link>
+        </div>
+        <div className='user-info'>
+            <div>Hello! {name} you have successfully Logged in </div>
+        </div>
+        <button className='logout-button' onClick={() =>setFlag(0)}>LOGOUT</button>
+    </div>
+  )
+}
+export default Home;
